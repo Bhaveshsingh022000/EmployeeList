63cf9e02363350f78beee0eb76963453e0399a2b
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-11-13 15:48:44 +0530

    added save button

--- src/Components/Employee/Employee.js
+++ src/Components/Employee/Employee.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt, faEdit, faSave } from '@fortawesome/free-solid-svg-icons';
 import InputField from '../UI/Input/Input';
 
 class Employee extends Component {
@@ -17,7 +17,7 @@ class Employee extends Component {
                 <p>
                     {this.state.showEdit ? <InputField changed={(event)=>this.props.changeEdit(event,this.props.inputIndex)} val={this.props.name} /> : this.props.name}
                     <span><FontAwesomeIcon icon={faTrashAlt} color="red" /></span>
-                    <span onClick={this.toggleEdit}><FontAwesomeIcon icon={faEdit} color="blue" /></span>
+                    {this.state.showEdit ? <span><FontAwesomeIcon icon={faSave} color="green" onClick={this.toggleEdit} /></span> : <span onClick={this.toggleEdit}><FontAwesomeIcon icon={faEdit} color="blue" /></span>}
                 </p>
             </div>
         );
