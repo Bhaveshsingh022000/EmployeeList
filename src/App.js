8e539040d23aa8b3b472694abd710899601f303c
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-11-13 11:18:48 +0530

    added input field component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-import Employees from './Components/Employees';
+import Employees from './Components/Employees/Employees';
+import InputField from './Components/UI/Input/Input';
 
 class App extends Component {
   state = {
     name:["Bhavesh","Test naame"]
   }
 
+  
 
   render() {
     let employee = this.state.name.map(el =>{
@@ -14,6 +16,7 @@ class App extends Component {
     })
     return (
       <div>
+        <InputField /> 
         {employee}
       </div>
     );
---
+++ src/Components/UI/Input/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const inputField = (props) => {
+    return (
+        <div>
+            <input type="text" value="" />
+        </div>
+    );
+}
+export default inputField;
