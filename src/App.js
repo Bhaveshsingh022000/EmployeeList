fa4119fc96df7b4f09cd1b9d2cc84cec04932016
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-11-13 12:17:34 +0530

    added add employee handler

--- src/App.js
+++ src/App.js
@@ -5,27 +5,37 @@ import InputField from './Components/UI/Input/Input';
 
 class App extends Component {
   state = {
-    employees:[
-      {id:"1", name:"bahvesh"}
-    ],
-    tempName: ""
+    employees:[],
+    tempName: "",
+    tempId: 0
   }
 
   inputChangeHandler = (event)=>{
-    const name = event.target.value;
-    const e = [...this.state.employees];
-    e[0].name = name;
-    this.setState({employees: e});
+    // const name = event.target.value;
+    // const e = [...this.state.employees];
+    // e[0].name = name;
+    // this.setState({employees: e});
     // console.log(this.state.tempName);
+    this.setState({tempName: event.target.value});
+  }
+
+  addEmployeeHandler = ()=>{
+    const employee = [...this.state.employees];
+    employee.push({id: Number(this.state.tempId + 1) , name: this.state.tempName});
+    this.setState({employees: employee, tempId:Number(this.state.tempId + 1)});
   }
 
   render() {
-    let employee = this.state.employees.map((el,index) =>{
-      return <Employees key={this.state.employees[index].id} name={this.state.employees[index].name} />
-    })
+    let employee = null;
+    if(this.state.employees.length !== 0){
+      employee = this.state.employees.map((el,index) =>{
+        return <Employees key={this.state.employees[index].id} name={this.state.employees[index].name} />
+      })
+    }
+    
     return (
       <div>
-        <InputField change={this.inputChangeHandler}  /> <button>Add Employee</button>
+        <InputField change={this.inputChangeHandler}  /> <button onClick={this.addEmployeeHandler}>Add Employee</button>
         {employee}
       </div>
     );
