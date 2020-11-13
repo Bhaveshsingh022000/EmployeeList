19a20237f6ab9f96ff19c4caa2785460d38055c5
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-11-13 22:26:52 +0530

    added validation to edit form

--- src/Containers/Employee/Employee.js
+++ src/Containers/Employee/Employee.js
@@ -6,25 +6,74 @@ import { faTrashAlt, faEdit, faSave, faPhone } from '@fortawesome/free-solid-svg
 import InputField from '../../Components/UI/Input/Input';
 
 class Employee extends Component {
+
+    constructor(props) {
+        super(props);
+        this.emailRef = React.createRef();
+        this.phoneRef = React.createRef();
+        this.nameRef = React.createRef();
+        this.designationRef = React.createRef();
+    }
+
+
     state = {
         showEdit: false
     }
-    toggleEdit = () => {
-        this.setState((prevState, nextProps) => { return ({ showEdit: !prevState.showEdit }) })
+
+
+    openEdit = () => {
+        this.setState((prevState, nextProps) => { return ({ showEdit: !prevState.showEdit }) });
     }
+
+    closeEdit = ()=>{
+        if(this.validate()){
+            this.setState((prevState, nextProps) => { return ({ showEdit: !prevState.showEdit }) });
+        }
+    }
+
+
+    validate = () => {
+        if (this.nameRef.current.value === "") {
+            alert("Name Cannot Be Empty");
+            this.nameRef.current.focus();
+            return false;
+        }
+        if (!this.emailRef.current.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+            alert("Invalid Email");
+            this.emailRef.current.focus();
+            return false;
+        }
+        if (this.phoneRef.current.value.length !== 10) {
+            alert(`Phone Number Should be of 10 digits, You have entered ${this.phoneRef.current.value.length} digits`);
+            this.phoneRef.current.focus();
+            return false;
+        }
+        if (this.designationRef.current.value === "") {
+            alert("Designation Cannot Be Empty");
+            this.designationRef.current.focus();
+            return false;
+        }
+        return true;
+    }
+
+
     render() {
+        let id = this.props.id;
+        if (id < 10) {
+            id = "0" + id;
+        }
         return (
             <div className={classes.Main}>
-                {this.state.showEdit ? <span className={classes.EditBtn} onClick={this.toggleEdit} ><FontAwesomeIcon icon={faSave} color="green" /></span> : <span className={classes.EditBtn} onClick={this.toggleEdit}><FontAwesomeIcon icon={faEdit} color="blue" /></span>}
-                
-                 <span>ID - {this.props.id}</span><br />
-                <span>Name - {this.state.showEdit ? <InputField changed={this.props.changeName} val={this.props.name} /> : this.props.name}</span><br />
-                <span>Email - {this.state.showEdit ? <InputField changed={this.props.changeEmail} val={this.props.email} /> : this.props.email}</span><br />
-                <span>Phone - {this.state.showEdit ? <InputField changed={this.props.changePhone} val={this.props.phone} /> : this.props.phone}</span><br />
-                <span>Designation - {this.state.showEdit ? <InputField changed={this.props.changeDesignation} val={this.props.designation} /> : this.props.designation}</span>
-                
+                {this.state.showEdit ? <span className={classes.EditBtn} onClick={this.closeEdit} ><FontAwesomeIcon icon={faSave} color="green" /></span> : <span className={classes.EditBtn} onClick={this.openEdit}><FontAwesomeIcon icon={faEdit} color="blue" /></span>}
+
+                <span>ID - {id}</span><br />
+                <span>Name - {this.state.showEdit ? <InputField refer={this.nameRef} inputType="text" changed={this.props.changeName} val={this.props.name} /> : this.props.name}</span><br />
+                <span>Email - {this.state.showEdit ? <InputField refer={this.emailRef} inputType="email" changed={this.props.changeEmail} val={this.props.email} /> : this.props.email}</span><br />
+                <span>Phone - {this.state.showEdit ? <InputField refer={this.phoneRef} inputType="number" changed={this.props.changePhone} val={this.props.phone} /> : this.props.phone}</span><br />
+                <span>Designation - {this.state.showEdit ? <InputField refer={this.designationRef} inputType="text" changed={this.props.changeDesignation} val={this.props.designation} /> : this.props.designation}</span>
+
                 <span className={classes.DeleteBtn} onClick={this.props.deleteEmployee}><FontAwesomeIcon icon={faTrashAlt} color="red" /></span>
-               
+
             </div>
         );
     }
